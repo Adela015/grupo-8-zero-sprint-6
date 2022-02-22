7acf829274e5ce8ad2cec928e97e753374db2f5b
Adela015 <ade.cervantes01@gmail.com>
2022-02-22 18:46:05 -0300

    Relaciones de Users

--- src/database/models/roles.js
+++ src/database/models/roles.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Roles.hasMany(models.Users,{
+        foreignKey: 'roles_ID',
+        as: 'Users'
+      });
     }
   }
   Roles.init({
