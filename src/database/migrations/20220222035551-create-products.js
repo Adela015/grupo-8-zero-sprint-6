82ad12002dbbe21419eff03fa47a7c7e7b5283e6
Adela015 <ade.cervantes01@gmail.com>
2022-02-22 10:57:50 -0300

    carpetas creadas y archivos js, faltan las relaciones

---
+++ src/database/migrations/20220222035551-create-products.js
@@ -0,0 +1,60 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.DECIMAL
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      stock_min: {
+        type: Sequelize.INTEGER
+      },
+      stock_max: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      artist: {
+        type: Sequelize.STRING
+      },
+      visibility: {
+        type: Sequelize.INTEGER
+      },
+      IDgenre: {
+        type: Sequelize.INTEGER
+      },
+      IDformat: {
+        type: Sequelize.INTEGER
+      },
+      ID_category: {
+        type: Sequelize.INTEGER
+      },
+      IDImages: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
